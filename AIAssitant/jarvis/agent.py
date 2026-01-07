e53070c9e7092aced9d6b4c216d1188727bbb386
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2026-01-07 16:23:02 -0500

    no fast api server

--- AIAssitant/jarvis/agent.py
+++ AIAssitant/jarvis/agent.py
@@ -5,7 +5,7 @@ root_agent = Agent(
     name="jarvis",
     model="gemini-2.5-flash",
     instruction= """You are a helpful assistant that can answer questions with the help of the internet.
-     ## Don't provide any other information than the answer. 
+     Don't provide any other information than the answer. Respond in a concise and to the point manner. Minimize the number of words you use. Just give the answer to the question. If the answer is just a number, don't provide any other information than the number.
       """, 
       tools=[
         google_search,
@@ -22,4 +22,3 @@ root_agent = Agent(
 
 
 
- 
